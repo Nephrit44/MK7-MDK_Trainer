dc7bf40cce92a480f0e0f1cfaf1daea2d26bf628
Nephrit44 <nephrit44@yandex.ru>
2026-05-08 15:32:10 +0300

    Добавлены чеклисты 5,6

--- JS/dbQestions.js
+++ JS/dbQestions.js
@@ -331,75 +331,417 @@ const dbQestions = [
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 3,
     cardQestion: "Подготовить ёмкость-контейнер с плотно прилегающей крышкой, проверить маркировку (название, концентрацию раствора, цель использования, дата)",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 4,
     cardQestion: "Залить в контейнер дезинфицирующий раствор нужной концентрации",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 5,
     cardQestion: "Погрузить термометр в дезинфицирующий раствор сразу после использования, (электронный термометр обрабатывается антисептической салфеткой)",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 6,
     cardQestion: "Выдержать время экспозиции в соответствии с методическими рекомендациями по применению дезинфицирующего раствора",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 7,
     cardQestion: "После экспозиции промыть термометр проточной водой",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 8,
     cardQestion: "Просушить, хранить в контейнере для термометров",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 9,
     cardQestion: "Снять нестерильные перчатки, сбросить в пакет для отходов класса Б",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 10,
     cardQestion: "Снять и поместить маску 3 -х слойную медицинскую одноразовую нестерильную в пакет для отходов класса «Б»",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 11,
     cardQestion: "Обработать руки гигиеническим способом (кожным антисептиком)",
     cardAction: "Выполнить",
     cardExemple: ""
   },
-    {
+  {
     cardID: 4,
     cardOrder: 12,
     cardQestion: "Сделать запись о выполненной процедуре в медицинской документации",
     cardAction: "Выполнить",
     cardExemple: ""
+  },
+
+  //Чеклист 5
+
+  {
+    cardID: 5,
+    cardOrder: 1,
+    cardQestion: "Взять кожный антисептик во флаконе с распылителем (или 2 антисептические салфетки)",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 2,
+    cardQestion: "Взять жгут венозный с застежкой (либо жгут резиновый)",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 3,
+    cardQestion: "Проверить срок годности кожного антисептика (спиртовых салфеток)",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 4,
+    cardQestion: "Проверить целостность упаковки салфеток",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 5,
+    cardQestion: "Провести гигиеническую обработку рук",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 6,
+    cardQestion: "Надеть средства индивидуальной защиты",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 7,
+    cardQestion: "Надеть нестерильные медицинские перчатки",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 8,
+    cardQestion: "Тщательно обработать жгут венозный с застежкой аэрозольным методом",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 9,
+    cardQestion: "Выдержать время экспозиции (30 секунд)",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 10,
+    cardQestion: "Повторно обработать жгут венозный с застежкой аэрозольным методом",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 11,
+    cardQestion: "Выдержать время экспозиции (30 секунд)",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 12,
+    cardQestion: "Дождаться полного высыхания жгута",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 13,
+    cardQestion: "Вскрыть упаковку одной спиртовой салфетки и протереть ею резиновый жгут по всей длине",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 14,
+    cardQestion: "Поместить салфетку в отходы класса Б",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 15,
+    cardQestion: "Поместить упаковку от салфетки в отходы класса А",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 16,
+    cardQestion: "Вскрыть упаковку второй спиртовой салфетки и протереть ею резиновый жгут по всей длине",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 17,
+    cardQestion: "Поместить салфетку в отходы класса Б",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 18,
+    cardQestion: "Поместить упаковки от салфеток в отходы класса А",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 19,
+    cardQestion: "Дождаться полного высыхания жгута",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 20,
+    cardQestion: "Снять перчатки, поместить их в отходы класса Б",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 21,
+    cardQestion: "Снять средства индивидуальной защиты, поместить их в отходы класса Б",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 5,
+    cardOrder: 22,
+    cardQestion: "Обработать руки гигиеническим способом",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+
+  //Чеклист 6
+
+  {
+    cardID: 6,
+    cardOrder: 1,
+    cardQestion: "Подготовить оснащение",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 2,
+    cardQestion: "Подготовить дезинфицирующее средство",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 3,
+    cardQestion: "Ограничить доступ к месту аварии",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 4,
+    cardQestion: "Надеть средства индивидуальной защиты",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 5,
+    cardQestion: "Надеть нестерильные медицинские перчатки",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 6,
+    cardQestion: "Провести дезинфекцию отходов на месте аварии",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 7,
+    cardQestion: "Провести сбор рассыпанных (разлитых) отходов по истечении времени экспозиции в пакет с соответствующей классу опасности отходов маркировкой",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 8,
+    cardQestion: "Закрыть и повторно маркировать упаковку, указав ФИО ответственного, дату, отделение",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 9,
+    cardQestion: "Обрабатать поверхность в месте рассыпания (розлива) отходов ветошью, смоченной в растворе дезинфицирующего средства согласно инструкции по применению к дезсредству",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 10,
+    cardQestion: "Транспортировать медицинские отходы в комнату временного хранения",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 11,
+    cardQestion: "Собрать твердые отходы в пакет с соответствующей классу опасности отходов маркировкой",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 12,
+    cardQestion: "Закрыть и повторно маркировать упаковку, указав ФИО ответственного, дату, отделение",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 13,
+    cardQestion: "Обработать поверхность в месте рассыпания (розлива) отходов ветошью, смоченной в растворе дезинфицирующего средства согласно инструкции по применению к дезсредству",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 14,
+    cardQestion: "Транспортировать медицинские отходы в комнату временного хранения",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 15,
+    cardQestion: "Если произошло повреждение тары для сбора ртутьсодержащих отходов провести демеркуризацию — удалить остатки ртути при помощи механических и физико-химических способов",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 16,
+    cardQestion: "Провести дезинфекцию отходов на месте аварии с использованием соответствующих дезинфицирующих средств (заливаю место аварии)",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 17,
+    cardQestion: "Произвести сбор рассыпанных (разлитых) отходов по истечении времени экспозиции в пакет с соответствующей классу опасности отходов и маркировкой",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 18,
+    cardQestion: "Закрыть и повторно маркировать упаковку, указав ФИО ответственного, дату, отделение",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 19,
+    cardQestion: "Обработать поверхность в месте рассыпания (розлива) отходов ветошью, смоченной в растворе дезинфицирующего средства согласно инструкции по применению дезсредства",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 20,
+    cardQestion: "Транспортировать медицинские отходы в комнату временного хранения",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 21,
+    cardQestion: "Снять средства индивидуальной защиты",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 22,
+    cardQestion: "Собрать спецодежду и средства индивидуальной защиты",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 23,
+    cardQestion: "Обработать перчатки салфеткой, смоченной дезинфицирующим раствором",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 24,
+    cardQestion: "Снять перчатки",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 25,
+    cardQestion: "Обработать руки гигиеническим способом",
+    cardAction: "Выполнить",
+    cardExemple: ""
+  },
+  {
+    cardID: 6,
+    cardOrder: 26,
+    cardQestion: "",
+    cardAction: "Выполнить",
+    cardExemple: "Сообщить об аварийной ситуации руководителю подразделения"
   },
 ];
 
